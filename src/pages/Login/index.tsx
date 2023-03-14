eff00c6608c7fa3bcc0ae9dd6cb542b20a395ff6
su100 <zxctys@naver.com>
2023-03-14 23:20:03 +0900

    feat: add login form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,56 @@
+import { useEffect, FormEvent, useState, ChangeEvent } from 'react';
+import { useLocation } from 'react-router-dom';
+
+interface LoginStateType {
+  username: string;
+  password: string;
+}
+
 function LoginPage() {
-  return <div>Login</div>;
+  const [state, setState] = useState<LoginStateType>({
+    username: '',
+    password: '',
+  });
+  const location = useLocation();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert('제출');
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((s) => ({ ...s, [name]: value }));
+  };
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const type = query.get('type');
+    if (!type) return;
+  }, [location]);
+
+  return (
+    <div>
+      <h1 className="m-10 text-xl">로그인</h1>
+      <form className="flex flex-col m-10 gap-2" onSubmit={handleSubmit}>
+        <input
+          className="border w-40 border-neutral-500"
+          type="text"
+          name="username"
+          onChange={handleInputChange}
+          placeholder="아이디"
+        />
+        <input
+          className="border w-40 border-neutral-500 "
+          type="password"
+          name="password"
+          onChange={handleInputChange}
+          placeholder="비밀번호"
+        />
+        <input className="w-20 bg-slate-600" type="submit" value="로그인" />
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
