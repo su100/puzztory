ab27a45c70c04eb1244b4fe2c2d00710867a16bc
su100 <zxctys@naver.com>
2023-03-22 22:19:17 +0900

    feat: 로그아웃 기능

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,19 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useAuthStore } from 'utils/auth';
 
 function Header() {
   const location = useLocation();
+  const { isLoggedIn, logout } = useAuthStore();
   return (
     <header>
       <Link to="/">메인</Link>
-      <Link to="/login" state={{ redirectPath: location.pathname }}>
-        로그인
-      </Link>
+      {isLoggedIn ? (
+        <button onClick={logout}>로그아웃</button>
+      ) : (
+        <Link to="/login" state={{ redirectPath: location.pathname }}>
+          로그인
+        </Link>
+      )}
     </header>
   );
 }
