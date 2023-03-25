0950020eb59816ebd7942b1edf8f6fde8123bfb5
su100 <zxctys@naver.com>
2023-03-25 22:38:34 +0900

    feat: 메인 페이지 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,29 @@
+import StoryCard from 'components/StoryCard';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { getStoryList, GET_STORY_LIST } from 'services/story';
+
 function MainPage() {
-  return <div>Main</div>;
+  const [search, setSearch] = useState('');
+  const { data } = useInfiniteQuery([...GET_STORY_LIST, search], () =>
+    getStoryList({ search }),
+  );
+
+  return (
+    <>
+      <div className="h-[15rem] md:h-[20rem] bg-sky-600">캐러셀</div>
+      <div className="p-4">
+        <input
+          className="px-2 py-1 w-[100%] rounded-lg border-gray-300 border"
+          placeholder="검색어를 입력하세요(제목, 내용)"
+        />
+        <h2 className="font-bold text-lg my-2">Puzzle</h2>
+        {data?.pages.map((p) =>
+          p.stories.map((s) => <StoryCard key={s.id} story={s} />),
+        )}
+      </div>
+    </>
+  );
 }
 
 export default MainPage;
