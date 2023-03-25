203a68042e269b433e8cb33e9df54afdf916791f
su100 <zxctys@naver.com>
2023-03-25 23:03:00 +0900

    fix: StoryCard 이미지 변경

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -9,7 +9,7 @@ function StoryCard({ story }: StoryCardProps) {
     <Link to={`/story/${story.id}`} className="flex align-center gap-2">
       <img
         className="rounded-[5px]"
-        src={story.background_image}
+        src={story.image}
         width="64"
         height="64"
         alt={`${story.title} 썸네일`}
