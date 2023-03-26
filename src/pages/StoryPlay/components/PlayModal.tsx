f6ac8cf3058e7f98362b4bc9f180259cc0161ba8
su100 <zxctys@naver.com>
2023-03-26 23:24:08 +0900

    fix: modal 모서리 둥글게

--- src/pages/StoryPlay/components/PlayModal.tsx
+++ src/pages/StoryPlay/components/PlayModal.tsx
@@ -8,7 +8,7 @@ interface PlayModalProps {
 function PlayModal({ children, handleClose }: PlayModalProps) {
   return (
     <div className="fixed w-full h-screen bg-black bg-opacity-30 top-0 left-0 flex justify-center items-center">
-      <div className="relative bg-white w-[80%] p-10">
+      <div className="relative rounded-md bg-white w-[80%] p-10">
         <button className="absolute right-3 top-2" onClick={handleClose}>
           X
         </button>
