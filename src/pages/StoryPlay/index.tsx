55b2e2d7a7283245f8f7030735b6e1a510e04190
su100 <zxctys@naver.com>
2023-03-26 23:01:09 +0900

    feat: 플레이 정답 제출 기능 및 모달 추가

--- src/pages/StoryPlay/index.tsx
+++ src/pages/StoryPlay/index.tsx
@@ -1,43 +1,159 @@
-import { useQuery } from 'react-query';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { playStory } from 'services/story';
+import { useQuery, useMutation } from 'react-query';
+import { AxiosError } from 'axios';
+import useModal from 'utils/hooks/useModal';
+import QuestionSheet from './components/QuestionSheet';
+import AnswerModal from './components/AnswerModal';
+import { MessageRes } from 'services/type';
+import {
+  getStorySheet,
+  GET_STORY_SHEET,
+  playStory,
+  submitSheetAnswer,
+  SubmitSheetAnswerRes,
+} from 'services/story';
 
+const STALE_TIME = 5 * 1000;
+
+// TODO: 최적화 memo
 function StoryPlayPage() {
   const { id } = useParams();
-  const { data: sheet } = useQuery([], () => playStory(Number(id)));
+  const [sheetId, setSheetId] = useState<number>();
+  const [answer, setAnswer] = useState('');
+  const [answerReply, setAnswerReply] = useState('');
+  const [isWrong, setIsWrong] = useState(false);
+
+  const {
+    isModalOpen: isAnswerModalOpen,
+    openModal: openAnswerModal,
+    closeModal: closeAnswerModal,
+  } = useModal();
+
+  const { data: startSheet } = useQuery(
+    [...GET_STORY_SHEET, id, 'start'],
+    () => playStory(Number(id)),
+    { staleTime: STALE_TIME },
+  );
+
+  const { data } = useQuery(
+    [...GET_STORY_SHEET, id, sheetId],
+    () => getStorySheet(sheetId!),
+    { staleTime: STALE_TIME, enabled: !!sheetId },
+  );
+
+  const sheet = useMemo(() => data || startSheet, [data, startSheet]);
+
+  const { mutate: submitAnswer } = useMutation(submitSheetAnswer, {
+    onSuccess: (r: SubmitSheetAnswerRes) => {
+      if (!r.is_valid) {
+        setIsWrong(true);
+        return;
+      }
+      setIsWrong(false);
+      openAnswerModal();
+      console.log('모달 띄우고 확인 누르면 다음 시트 가져오기');
+    },
+    onError: (e: AxiosError<MessageRes>) => {
+      console.log('error:', e.response?.data.message);
+    },
+  });
+
+  const handleAnswerChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+  };
+
+  const handleSubmitAnswer = () => {
+    console.log('제출', answer);
+    if (answer.trim() === '') {
+      alert('정답을 입력해주세요');
+      return;
+    }
+
+    const sheet_id = sheet?.sheet_id || startSheet?.sheet_id;
+    if (!sheet_id) return;
+    submitAnswer({ sheet_id, answer });
+  };
+
+  const handleAnswerModal = () => {
+    openAnswerModal();
+  };
+
+  const handlePrevSheet = () => {
+    if (!sheet) return;
+    const { previous_sheet_infos } = sheet;
+    setSheetId(previous_sheet_infos[previous_sheet_infos.length - 1].sheet_id);
+  };
+
+  const handleNextSheet = () => {
+    console.log('handleNextSheet');
+    setSheetId(sheet?.next_sheet_id);
+  };
+
+  useEffect(() => {
+    if (sheet) {
+      setAnswer(sheet.answer || '');
+      setAnswerReply(sheet.answer_reply || '');
+    }
+  }, [sheet]);
+
   return (
     <div>
       <div className="px-2 py-3">hint icon</div>
-      <h1 className="px-2 h-10 bg-slate-400 leading-10 text-white font-bold text-sm text-center border-sky-600 border-t border-b">
-        {sheet?.title}
-      </h1>
-      <div className="relative flex justify-center bg-sky-100">
-        {sheet?.background_image && (
-          <img
-            src={sheet?.background_image}
-            alt={`${sheet?.title} 배경 이미지`}
-          />
-        )}
-        {sheet?.image && (
-          <img
-            className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2"
-            src={sheet?.image}
-            alt={`${sheet?.title} 이미지`}
-          />
-        )}
-      </div>
-      <h2 className="p-2 min-h-10 bg-slate-200 font-bold border-slate-300 border-t border-b">
-        {sheet?.question}
-      </h2>
+      <QuestionSheet sheet={sheet} />
       <div className="p-8 flex flex-col items-center justify-center gap-5 text-center">
         <input
           className="rounded-xl px-3 py-1 w-[50%] border border-slate-300"
+          value={answer}
+          onChange={handleAnswerChange}
+          disabled={sheet?.is_solved}
           placeholder="정답"
         />
-        <button className="w-[110px] px-2 py-1 bg-slate-400 rounded-md">
-          확인
+        {sheet?.is_solved ? (
+          <button
+            className="w-[110px] px-2 py-1 bg-slate-400 rounded-md"
+            onClick={handleAnswerModal}
+          >
+            답변 확인
+          </button>
+        ) : (
+          <button
+            className="w-[110px] px-2 py-1 bg-slate-400 rounded-md"
+            onClick={handleSubmitAnswer}
+          >
+            확인
+          </button>
+        )}
+        {isWrong && (
+          <span className="text-red-500 font-bold text-sm">
+            정답이 아니에요
+          </span>
+        )}
+      </div>
+      <div className="flex justify-center gap-3">
+        {sheet?.previous_sheet_infos && (
+          <button
+            className="w-[110px] px-2 py-1rounded-md rounded-md border border-slate-400"
+            onClick={handlePrevSheet}
+          >
+            이전
+          </button>
+        )}
+        <button
+          className="w-[110px] px-2 py-1 rounded-md border border-slate-400"
+          onClick={handleNextSheet}
+        >
+          다음
         </button>
       </div>
+      {isAnswerModalOpen && (
+        <AnswerModal
+          answerReply={answerReply}
+          answer={answer}
+          handleClose={closeAnswerModal}
+          handleNextSheet={handleNextSheet}
+        />
+      )}
     </div>
   );
 }
