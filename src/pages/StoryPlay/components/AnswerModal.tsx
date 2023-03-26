ee04ac4a1fe1bcf4329b2eae599a82a6b7c57a9b
su100 <zxctys@naver.com>
2023-03-26 22:59:38 +0900

    fix: AnswerModal 다음 버튼 클릭시 모달 닫기

--- src/pages/StoryPlay/components/AnswerModal.tsx
+++ src/pages/StoryPlay/components/AnswerModal.tsx
@@ -13,6 +13,11 @@ function AnswerModal({
   handleClose,
   handleNextSheet,
 }: AnswerModalProps) {
+  const handleNext = () => {
+    handleClose();
+    handleNextSheet();
+  };
+
   return (
     <PlayModal handleClose={handleClose}>
       <div className="p-4 border border-slate-300 rounded-sm">
@@ -22,7 +27,7 @@ function AnswerModal({
         <p className="my-4 text-sm text-green-600 font-bold">정답: {answer}</p>
         <button
           className="mx-auto px-3 py-1 rounded-md w-[100px] text-sm bg-slate-300"
-          onClick={handleNextSheet}
+          onClick={handleNext}
         >
           다음
         </button>
