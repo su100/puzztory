3e8685f125cb7b8d422a157303d3d590455ba21b
su100 <zxctys@naver.com>
2023-04-06 23:29:39 +0900

    fix: footer 폰트 굵기 수정

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="h-[120px] w-full flex flex-col justify-center px-[20px] bg-neutral-700 text-neutral-200">
+    <footer className="h-[120px] w-full flex flex-col justify-center px-[20px] bg-neutral-700 text-neutral-200 font-thin">
       <p>Created by su100</p>
       <p>Email: sykim0485@naver.com</p>
       <p>© 2023 puzztory. All rights reserved.</p>
