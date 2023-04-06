29fce424bf71d6c702437e786761a0adcdbd42bf
su100 <zxctys@naver.com>
2023-04-06 23:59:37 +0900

    fix: 메인 영역 높이 조절

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -76,7 +76,7 @@ function SignupPage() {
   };
 
   return (
-    <div className="w-[90%] max-w-[800px] mx-auto pb-[70px]">
+    <div className="w-[90%] max-w-[800px] mx-auto">
       <h1 className="title">회원가입</h1>
       <form className="flex flex-col my-10 gap-4" onSubmit={handleSubmit}>
         <Input
