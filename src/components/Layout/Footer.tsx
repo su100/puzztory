9a93ff3ea07437ec09852375dece12ccdfa66cb6
su100 <zxctys@naver.com>
2023-04-06 22:55:17 +0900

    feat: 푸터 스타일 추가

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="h-[120px] w-full flex flex-col justify-center px-[20px] bg-neutral-700 text-neutral-200">
+      <p>Created by su100</p>
+      <p>Email: sykim0485@naver.com</p>
+      <p>© 2023 puzztory. All rights reserved.</p>
+    </footer>
+  );
 }
 
 export default Footer;
