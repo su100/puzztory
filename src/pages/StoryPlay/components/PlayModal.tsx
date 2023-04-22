00166ec023dc1e46f5589a34cd2fb2b2482df787
su100 <zxctys@naver.com>
2023-04-22 13:22:59 +0900

    fix: 모달 기본 높이 수정, 백그라운드 스크롤 방지

--- src/pages/StoryPlay/components/PlayModal.tsx
+++ src/pages/StoryPlay/components/PlayModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface PlayModalProps {
   children: React.ReactNode;
@@ -6,12 +6,21 @@ interface PlayModalProps {
 }
 
 function PlayModal({ children, handleClose }: PlayModalProps) {
+  const closeModal = () => {
+    document.body.style.overflow = 'visible';
+    handleClose();
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+  }, []);
+
   return (
     <div className="fixed w-full h-screen bg-black bg-opacity-30 top-0 left-0 flex justify-center items-center">
-      <div className="relative rounded-md bg-white w-[80%] h-[70%] p-10">
+      <div className="relative rounded-md bg-white w-[80%] max-h-[70%] p-10">
         <button
           className="absolute right-4 top-3 font-bold text-gray-700"
-          onClick={handleClose}
+          onClick={closeModal}
         >
           ✕
         </button>
