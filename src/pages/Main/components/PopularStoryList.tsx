8c138ff1bbb1d3dd20e805def8153ffaec703748
su100 <zxctys@naver.com>
2023-04-22 14:58:20 +0900

    fix: 스토리 이미지 기본값 설정

--- src/pages/Main/components/PopularStoryList.tsx
+++ src/pages/Main/components/PopularStoryList.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { GET_POPULAR_STORY_LIST, getPopularStoryList } from 'services/story';
+import defaultImg from 'assets/img/default.png';
 
 function PopularStoryList() {
   const { data } = useQuery(GET_POPULAR_STORY_LIST, getPopularStoryList);
@@ -17,7 +18,7 @@ function PopularStoryList() {
               <img
                 width="120"
                 height="120"
-                src={s.image}
+                src={s.image || defaultImg}
                 alt={`${s.story_id} 썸네일`}
               />
               <h3 className="text-sm font-medium ellipsis">{s.title}</h3>
