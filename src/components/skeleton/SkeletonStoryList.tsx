1cab46d5e65464afe2cadb3492f97fab02f03792
su100 <zxctys@naver.com>
2023-04-24 20:18:45 +0900

    fix: SkeletonStoryList 스타일 변수화

--- src/components/skeleton/SkeletonStoryList.tsx
+++ src/components/skeleton/SkeletonStoryList.tsx
@@ -1,15 +1,20 @@
+export const SKELETON_STYLE = 'bg-slate-300 relative';
+
+export const SKELETON_ANIMATION_STYLE =
+  'absolute top-0 left-0 right-0 bottom-0 animate-pulse bg-gray-200';
+
 function SkeletonStory() {
   return (
     <div className="flex items-center gap-2 relative">
-      <div className="rounded-[5px] w-[64px] h-[64px] bg-slate-300 relative">
-        <div className="absolute top-0 left-0 right-0 bottom-0 animate-pulse bg-gray-200 rounded-[5px]"></div>
+      <div className={`${SKELETON_STYLE} rounded-[5px] w-[64px] h-[64px]`}>
+        <div className={`${SKELETON_ANIMATION_STYLE} rounded-[5px]`}></div>
       </div>
       <div className="w-[calc(100%-64px)] relative">
-        <h3 className="mb-1 w-4/5 h-4 bg-slate-300 rounded-sm relative">
-          <span className="absolute top-0 left-0 right-0 bottom-0 animate-pulse bg-gray-200 rounded-sm"></span>
+        <h3 className={`${SKELETON_STYLE} mb-1 w-4/5 h-4 rounded-sm`}>
+          <span className={`${SKELETON_ANIMATION_STYLE} rounded-sm`}></span>
         </h3>
-        <p className="w-full h-9 bg-slate-300 rounded-sm relative">
-          <span className="absolute top-0 left-0 right-0 bottom-0 animate-pulse bg-gray-200 rounded-sm"></span>
+        <p className={`${SKELETON_STYLE} w-full h-9 rounded-sm`}>
+          <span className={`${SKELETON_ANIMATION_STYLE} rounded-sm`}></span>
         </p>
       </div>
     </div>
